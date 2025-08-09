5f1a2b3a814c24cc130ba153ea8b516c6d8a5ff4
LCO-L <zest9908@naver.com>
2025-08-09 13:28:10 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '',
-  trailingSlash: true
+  distDir: 'docs',
 };
+
 export default nextConfig;
+
